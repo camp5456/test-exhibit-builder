3ed9706ce53335b966bbac193f80cb6974377923
Twila Smith <7118907+camp5456@users.noreply.github.com>
2025-05-23 15:54:44 -0500

    1

--- assets/js/display_template.js
+++ assets/js/display_template.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', function () {
-  switch (display_template) {
-    case "iiif":
-      loadTemplate("iiif");
-      break;
-    default:
-      loadTemplate("default");
+  if (typeof item !== 'undefined') {
+    switch (item.display_template) {
+      case "iiif":
+        loadTemplate("iiif");
+        break;
+      default:
+        loadTemplate("default");
+    }
   }
 });
